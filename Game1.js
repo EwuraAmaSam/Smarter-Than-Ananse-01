560c94b4c08119ed9dd3d53faa2a7f93fa33cab9
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2022-10-12 20:25:02 +0000

    Change file name

--- Game1.js
+++ Game1.js
@@ -1,3 +1,5 @@
+alert("This is a match between you and Ananse. The first to cross 300 points wins. All the best.");
+
 const h1 = document.querySelector('h1');
 const h2 = document.querySelector('h2');
 const button1 = document.querySelector('.button1');
@@ -22,23 +24,29 @@ let value2 = 0;
 // button1.addEventListener('click',addAnanse);
 
 function addYou(){
-    
+    // Generating random numbers for Ananse
     let possible = [1,2,3,4,-5,0,6,7,8,9,10,0,-11,12,13,14,15,16,-17,18,0,19,20,21]
     let random = Math.floor(Math.random() * possible.length)
     let number = possible[random]
     value1 += number;
     h1.textContent = value1;
-
-
-
-
+    // Generating random numbers for the player
 
     let possible2 = [1,0,2,3,4,5,6,7,-8,9,0,10,-11,12,13,14,-15,0,16,17,18,19,-20,21]
     let random2 = Math.floor(Math.random()*possible2.length)
     let number2 = possible2[random2]
     value2 += number2;
     h2.textContent = value2;
+
+    if (value2 > 300){
+        alert("Ananse has won🕷. Refresh the browser to play again.")
+    }
+
+    if (value1 > 300){
+        alert("You won🎉. Refresh the browser to play again.")
+    }
 }
 
 button2.addEventListener('click',addYou);
+
 
